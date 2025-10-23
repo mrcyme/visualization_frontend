e8b659061ee314cffc9b421342c4bc5c22d368d1
mrcyme <simeonmcl@gmail.com>
2025-10-23 14:04:06 +0200

    images

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '..', '.env') });
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4002;
 
 app.use(compression());
 app.use(cors());
